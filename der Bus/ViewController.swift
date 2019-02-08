b494474eda505ec89f022eebcc5d43c490ff9d1b
Shantanu Dutta <shantanudutta.bharadwaj@gmail.com>
2019-02-08 21:07:40 +0530

    Initial Commit

---
+++ der Bus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  der Bus
+//
+//  Created by Shantanu Dutta on 08/02/19.
+//  Copyright © 2019 Shantanu Dutta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
